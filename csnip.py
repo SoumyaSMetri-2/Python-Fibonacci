c114cae0c710304c727fbd083046352d6520517a
SoumyaSMetri-2 <69234351+SoumyaSMetri-2@users.noreply.github.com>
2020-09-13 23:47:14 +0530

    Add files via upload

---
+++ csnip.py
@@ -0,0 +1,20 @@
+lst = list()
+lst.append('soumya')
+lst.append(55)
+lst.append('riuvjn')
+lst.append(101)
+lst.append(342)
+print(lst)
+lst[4]= 148
+print(lst)
+tap=('A',22,44,'B',111,'C',6,'Z',9)
+print(tap)
+print(tap[3:6])
+tic = dict()
+tic['clock']=6
+tic['money']=145
+tic['name']='soumya'
+tic['choclate']=1000
+print(tic)
+tic.pop('money')
+print(tic)
